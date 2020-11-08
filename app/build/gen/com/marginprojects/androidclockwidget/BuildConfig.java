ac1fd9fbdc42a60df8dc802f35719c85a14964e8
hungry-thom <tmargesson@gmail.com>
2020-11-07 19:52:52 -0600

    initial commit

---
+++ app/build/gen/com/marginprojects/androidclockwidget/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.marginprojects.androidclockwidget;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
