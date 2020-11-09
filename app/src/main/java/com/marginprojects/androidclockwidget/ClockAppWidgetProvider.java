6cba4f3a5418c32da4f249e6b3e23ed2833e4d7e
hungry-thom <tmargesson@gmail.com>
2020-11-09 15:42:28 -0600

    remote init

---
+++ app/src/main/java/com/marginprojects/androidclockwidget/ClockAppWidgetProvider.java
@@ -0,0 +1,31 @@
+package com.marginprojects.androidclockwidget;
+import android.*;
+import android.app.*;
+import android.appwidget.*;
+import android.content.*;
+import android.widget.*;
+
+public class ClockAppWidgetProvider extends AppWidgetProvider
+ {
+
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.clock_appwidget);
+            views.setOnClickPendingIntent(R.id.button_widget, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
